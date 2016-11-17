e8e1111b487a65f85a8a19060bc228460ceb64ff
Fredrik Christoffer Chrislock <Fredrik Christoffer Chrislock>
2016-11-17 13:44:49 +0100

    Updated and working

--- task_2.2/angle.m
+++
@@ -1,7 +0,0 @@
-function phi = angle(v1,v2)
-
-v_dot = v1*v2';                                                 % Dot product
-v_abs = (v1(1).^2+v1(2).^2).^0.5*(v2(1).^2+v2(2).^2).^0.5;      % Absolute product
-
-phi = acos(v_dot/v_abs);
-
--- task_2.2/path_length.m
+++
@@ -1,3 +0,0 @@
-function length = path_length(p1, p2)
-d_v = p2-p1;
-length = norm(d_v);
